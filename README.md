f6fe2152602ecdb17fcf35ae6ab68a49927ad8c5
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-04-12 19:16:31 -0400

    feat: Load LDTK level
    Signed-off-by: Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# bevy-platformer
+
+## Credits
+
+Following the [LDTK integration tutorial](https://trouv.github.io/bevy_ecs_ldtk/v0.11.0/tutorials/tile-based-game/create-your-ldtk-project.html), I am using:
+
+- [SunnyLand](https://ansimuz.itch.io/sunny-land-pixel-game-art), a texture pack by Ansimuz, licensed under [CC0 1.0](https://creativecommons.org/publicdomain/zero/1.0/)
--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,19 @@
 
 use avian2d::{math::*, prelude::*};
 use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins((
-            DefaultPlugins,
+            DefaultPlugins.set(ImagePlugin::default_nearest()),
             PhysicsPlugins::default().with_length_unit(20.0),
             PhysicsDiagnosticsPlugin,
             PhysicsDiagnosticsUiPlugin,
         ))
+        .add_plugins(LdtkPlugin)
         .insert_resource(Gravity(Vector::NEG_Y * 1000.0))
+        .insert_resource(LevelSelection::index(0))
         .add_systems(Startup, setup)
         .run();
 }
@@ -35,8 +38,21 @@ fn setup(
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
+    asset_server: Res<AssetServer>,
 ) {
-    commands.spawn(Camera2d);
+    commands.spawn((
+        Camera2d,
+        Projection::Orthographic(OrthographicProjection {
+            scale: 0.5,
+            ..OrthographicProjection::default_2d()
+        }),
+        Transform::from_xyz(1280.0 / 4.0, 720.0 / 4.0, 0.0),
+    ));
+
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("tile-based-game.ldtk").into(),
+        ..Default::default()
+    });
 
     let player_size = Vector::new(20.0, 20.0);
     let player_mesh = meshes.add(Rectangle::from_size(player_size.f32()));
@@ -49,6 +65,7 @@ fn setup(
         LockedAxes::ROTATION_LOCKED,
         Restitution::ZERO.with_combine_rule(CoefficientCombine::Min),
         Collider::rectangle(player_size.x, player_size.y),
+        Position::from_xy(500.0, 200.0),
         Player,
     ));
 }
